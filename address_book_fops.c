e508347cde17c1a6e862fe664dce464cc5c185c8
Gabrielle David <gtdavid@cpp.edu>
2021-10-19 00:30:55 -0700

    implemented load_file

--- address_book_fops.c
+++ address_book_fops.c
@@ -11,10 +11,11 @@
 Status load_file(AddressBook *address_book)
 {
 	int ret;
-
+	struct stat buffer;
 	/* 
 	 * Check for file existance
 	 */
+	ret = stat(DEFAULT_FILE, &buffer);
 
 	if (ret == 0)
 	{
@@ -22,10 +23,20 @@ Status load_file(AddressBook *address_book)
 		 * Do the neccessary step to open the file
 		 * Do error handling
 		 */ 
+		address_book->fp = fopen(DEFAULT_FILE, "r");
+		if (address_book->fp == NULL)
+		{
+			return e_fail;
+		}
+		else
+		{
+			fclose(address_book->fp);
+		}
 	}
 	else
 	{
 		/* Create a file for adding entries */
+		address_book->fp = fopen(DEFAULT_FILE, "w");
 	}
 
 	return e_success;
